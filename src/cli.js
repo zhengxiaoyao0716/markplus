9c11d2b155abca7ab7883cf801b08211d2af6da4
zhengxiaoyao0716 <1499383852@qq.com>
2018-01-15 14:53:57 +0800

    modify the plugin register method.

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,11 @@ import * as babel from 'babel-core';
 import commander from 'commander';
 
 import pkg from './../package.json';
-import Markplus, { use } from './core';
+import Markplus from './core';
 import PluginStyleDefault from './plugin/style-default';
 
 const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, './../.babelrc'), 'utf-8'));
 const launch = name => `<div id="markplus" class="Markplus"></div><script>${name}.default(document.querySelector('#markplus'));</script>`;
-use(PluginStyleDefault);
 
 const transform = (code, name) => babel.transform(code, { ...babelrc, plugins: ['transform-es2015-modules-umd'], filename: name });
 
@@ -18,7 +17,8 @@ const compile = () => {
     if (!input) {
         throw new Error('missing input file.');
     }
-    Markplus.from(fs.readFileSync(input, 'utf-8'))
+    const name = commander.args[1];
+    Markplus.from(fs.readFileSync(input, 'utf-8'), name, { plugin: [PluginStyleDefault] })
         .then(mp => ({ ...mp, code: mp.code() }))
         .then(mp => commander.transform ? ({ ...mp, ...transform(mp.code, mp.name) }) : mp)
         .then(({ code, name, plugin }) => commander.js ? code : [
@@ -27,14 +27,14 @@ const compile = () => {
             launch(name),
             '',
         ].join('\n'))
-        .then(output => {
+        .then(output =>
             commander.out ?
                 fs.writeFileSync(commander.out, output, 'utf-8')
-                : console.log(output); // eslint-disable-line no-console
-        }).catch(console.error); // eslint-disable-line no-console
+                : console.log(output) // eslint-disable-line no-console
+        ).catch(console.error); // eslint-disable-line no-console
 };
 
-commander.version(pkg.version).usage('[options] <file ...>');
+commander.version(pkg.version).usage('[options] INPUT <name> ...');
 [
     ['--html', 'Compile to html'],
     ['--js', 'Compile to javascript'],
--- src/core.js
+++ src/core.js
@@ -7,58 +7,61 @@ import PluginRenderPromise from './plugin/render-promise';
 import pkg from './../package.json';
 
 const Render = fs.readFileSync(path.join(__dirname, './../src/Render.js'), 'utf-8');
-const plugins = [
-    (self: Markplus) => {
-        if (!self.name) {
-            const firstH1: Types.Header = self.elements.find(ele => ele instanceof Types.Header && ele.level == 1);
-            self.name = firstH1 ? `${firstH1.content}` : `_${new Date().getTime()}`;
-        }
-        return {
-            head: () => `<!-- Markplus: ${self.name} -->`,
-            code: () => `${Render}Markplus.__defineGetter__('version', () => '${pkg.version}');\n`,
-            dump: () => [
-                `\nexport const name = '${self.name}';`,
-                ...self.elements.map(ele => ele.dump()),
-            ].join('\n'),
-        };
-    },
-];
-const pluginNames = new Set();
-export const use = (
-    plugin: (self: Markplus) => {
-        head: () => string,
-        code: () => string,
-        dump: () => string,
+const CorePlugin = (self: Markplus) => {
+    if (!self.name) {
+        const firstH1: Types.Header = self.elements.find(ele => ele instanceof Types.Header && ele.level == 1);
+        self.name = firstH1 ? `${firstH1.content}` : `_${new Date().getTime()}`;
     }
-) => {
-    const name = plugin.name;
-    if (!name) {
-        throw new Error('Invalid plugin.');
-    }
-    if (pluginNames.has(name)) {
-        console.warn(new Error(`plugin name (${name}) conflict.`).stack.replace(/^Error/, 'Warning')); // eslint-disable-line no-console
-    }
-    pluginNames.add(name);
-    plugins.push(plugin);
+    return {
+        head: () => `<!-- Markplus: ${self.name} -->`,
+        code: () => `${Render}Markplus.__defineGetter__('version', () => '${pkg.version}');\n`,
+        dump: () => [
+            `\nexport const name = '${self.name}';`,
+            ...self.elements.map(ele => ele.dump()),
+        ].join('\n'),
+    };
 };
-
-[
+const corePlugins = [
+    CorePlugin,
     PluginRenderCode,
     PluginRenderPromise,
-].forEach(use);
+];
+export type Plugin = (self: Markplus) => {
+    head: () => string,
+    code: () => string,
+    dump: () => string,
+};
+export type Options = {
+    plugin: [string | Plugin],
+};
 
 export default class Markplus {
-    static from = (content: string | [string]): Promise<Markplus> => new Promise((resolve, reject) => {
+    static from = (content: string | [string], name?: string, opts?: Options): Promise<Markplus> => new Promise((resolve, reject) => {
         const lines = content instanceof Array ? content : content.split(content.endsWith('\r\n') ? '\r\n' : '\n');
         const elements = Parser.parse(lines);
-        resolve(new Markplus(elements));
+        resolve(new Markplus(elements, name || '', opts || {}));
     });
 
-    constructor(elements: [Types.Element], name?: string) {
+    constructor(elements: [Types.Element], name: string, opts: Options) {
         this.elements = elements;
         this.name = name;
-        const ps = plugins.map(plugin => plugin(this));
-        this.plugin = (action: string): string => ps.map(p => p[action] && p[action]()).filter(code => code != null);
+
+        this.plugin = (() => {
+            const pluginNames = new Set();
+            const plugins = [...corePlugins, ...opts.plugin || []].map(payload => {
+                const plugin: Plugin = payload instanceof Function ? payload : require(`markplus-plugin-${payload}`).default;
+                const name = plugin.name;
+                if (!name) {
+                    throw new Error('Invalid plugin.');
+                }
+                if (pluginNames.has(name)) {
+                    console.warn(new Error(`plugin name (${name}) conflict.`).stack.replace(/^Error/, 'Warning')); // eslint-disable-line no-console
+                }
+                pluginNames.add(name);
+                return plugin;
+            }).map(plugin => plugin(this));
+            return (action: string): string => plugins.map(p => p[action] && p[action]()).filter(code => code != null);
+        })();
     }
 
     code = () => ([
